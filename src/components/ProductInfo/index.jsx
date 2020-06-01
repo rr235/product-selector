bf01f92dd07afe348bc0ceffe949e72811f81ef2
Rahul <rash235r@gmail.com>
2020-06-01 15:52:55 +0200

    Added some styling for product info

--- src/components/ProductInfo/index.jsx
+++ src/components/ProductInfo/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { string } from 'prop-types';
+import styles from './styles.scss';
 
 const ProductInfo = ({ title, imageUrl }) => (
-  <div>
-    <img src={imageUrl} alt={title} />
+  <div className={styles.container}>
+    <img src={imageUrl} alt={title} className={styles.image} />
     <h1>{title}</h1>
   </div>
 );
--- src/views/Results/index.jsx
+++ src/views/Results/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import ProductInfo from '../../components/ProductInfo';
+import styles from './styles.scss';
 import { postAnswers } from './data';
 import { selectAnswers } from '../../reducers/answers';
 
@@ -16,7 +17,7 @@ const Results = () => {
   }, [answers]);
 
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Congratulations!</h1>
       <p>Based on your selection we have decided on {result.name}</p>
       <ProductInfo title={result.name} imageUrl={result.image} />
